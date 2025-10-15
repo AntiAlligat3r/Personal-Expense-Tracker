3a1bc3d2f647d9e8652d37cab38c22ea373cfadd
antialligat3r <fallschirmjager99@gmail.com>
2025-10-16 00:14:12 +0200

    Add project files.

---
+++ Personal Expense Tracker/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace Personal_Expense_Tracker
+{
+    partial class frmCreationLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dbExpenseTrackerDataSet1 = new Personal_Expense_Tracker.dbExpenseTrackerDataSet();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.hplkResetPassword = new System.Windows.Forms.LinkLabel();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.pnlAccountCreation = new System.Windows.Forms.Panel();
+            this.ckbxShowPassword = new System.Windows.Forms.CheckBox();
+            this.lblErrorEmail = new System.Windows.Forms.Label();
+            this.lblErrorPassword = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblConfirmPassword = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.btnShowAccountPanel = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dbExpenseTrackerDataSet1)).BeginInit();
+            this.pnlAccountCreation.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.pnlLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dbExpenseTrackerDataSet1
+            // 
+            this.dbExpenseTrackerDataSet1.DataSetName = "dbExpenseTrackerDataSet";
+            this.dbExpenseTrackerDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(162, 160);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "&Log in";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.Location = new System.Drawing.Point(100, 115);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(93, 23);
+            this.btnCreateAccount.TabIndex = 1;
+            this.btnCreateAccount.Text = "&Create Account";
+            this.btnCreateAccount.UseVisualStyleBackColor = true;
+            this.btnCreateAccount.Click += new System.EventHandler(this.btnCreateAccount_Click);
+            // 
+            // hplkResetPassword
+            // 
+            this.hplkResetPassword.AutoSize = true;
+            this.hplkResetPassword.Location = new System.Drawing.Point(159, 186);
+            this.hplkResetPassword.Name = "hplkResetPassword";
+            this.hplkResetPassword.Size = new System.Drawing.Size(84, 13);
+            this.hplkResetPassword.TabIndex = 2;
+            this.hplkResetPassword.TabStop = true;
+            this.hplkResetPassword.Text = "&Reset Password";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(3, 0);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.TabIndex = 3;
+            this.lblUsername.Text = "Username";
+            // 
+            // pnlAccountCreation
+            // 
+            this.pnlAccountCreation.Controls.Add(this.button1);
+            this.pnlAccountCreation.Controls.Add(this.ckbxShowPassword);
+            this.pnlAccountCreation.Controls.Add(this.lblErrorEmail);
+            this.pnlAccountCreation.Controls.Add(this.lblErrorPassword);
+            this.pnlAccountCreation.Controls.Add(this.tableLayoutPanel1);
+            this.pnlAccountCreation.Controls.Add(this.label2);
+            this.pnlAccountCreation.Location = new System.Drawing.Point(12, 12);
+            this.pnlAccountCreation.Name = "pnlAccountCreation";
+            this.pnlAccountCreation.Size = new System.Drawing.Size(619, 216);
+            this.pnlAccountCreation.TabIndex = 5;
+            this.pnlAccountCreation.Visible = false;
+            // 
+            // ckbxShowPassword
+            // 
+            this.ckbxShowPassword.AutoSize = true;
+            this.ckbxShowPassword.Location = new System.Drawing.Point(155, 182);
+            this.ckbxShowPassword.Name = "ckbxShowPassword";
+            this.ckbxShowPassword.Size = new System.Drawing.Size(102, 17);
+            this.ckbxShowPassword.TabIndex = 9;
+            this.ckbxShowPassword.Text = "Show Password";
+            this.ckbxShowPassword.UseVisualStyleBackColor = true;
+            this.ckbxShowPassword.Click += new System.EventHandler(this.ckbxShowPassword_Click);
+            // 
+            // lblErrorEmail
+            // 
+            this.lblErrorEmail.AutoSize = true;
+            this.lblErrorEmail.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorEmail.Location = new System.Drawing.Point(344, 127);
+            this.lblErrorEmail.Name = "lblErrorEmail";
+            this.lblErrorEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblErrorEmail.TabIndex = 8;
+            this.lblErrorEmail.Text = "label5";
+            this.lblErrorEmail.Visible = false;
+            // 
+            // lblErrorPassword
+            // 
+            this.lblErrorPassword.AutoSize = true;
+            this.lblErrorPassword.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorPassword.Location = new System.Drawing.Point(344, 66);
+            this.lblErrorPassword.Name = "lblErrorPassword";
+            this.lblErrorPassword.Size = new System.Drawing.Size(35, 13);
+            this.lblErrorPassword.TabIndex = 6;
+            this.lblErrorPassword.Text = "label3";
+            this.lblErrorPassword.Visible = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.5F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnCreateAccount, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.txtConfirmPassword, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtUsername, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblPassword, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblUsername, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtPassword, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblConfirmPassword, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtEmail, 1, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(55, 38);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.12281F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.87719F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(283, 145);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Email";
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Location = new System.Drawing.Point(100, 60);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(180, 20);
+            this.txtConfirmPassword.TabIndex = 8;
+            this.toolTip1.SetToolTip(this.txtConfirmPassword, "Must be the same as Password\r\n");
+            this.txtConfirmPassword.UseSystemPasswordChar = true;
+            this.txtConfirmPassword.TextChanged += new System.EventHandler(this.txtConfirmPassword_TextChanged);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(100, 3);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(180, 20);
+            this.txtUsername.TabIndex = 7;
+            this.txtUsername.Tag = "";
+            this.toolTip1.SetToolTip(this.txtUsername, "30 Maximum Allowed characters. No special Characters allowed");
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(3, 28);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(100, 31);
+            this.txtPassword.MaxLength = 30;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(180, 20);
+            this.txtPassword.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.txtPassword, " Maximum of 30 Characters. Must Include:\r\n\r\nLower Case\r\nUpper Case\r\nSpecial Chara" +
+        "cter\r\nNumber");
+            this.txtPassword.UseSystemPasswordChar = true;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // lblConfirmPassword
+            // 
+            this.lblConfirmPassword.AutoSize = true;
+            this.lblConfirmPassword.Location = new System.Drawing.Point(3, 57);
+            this.lblConfirmPassword.Name = "lblConfirmPassword";
+            this.lblConfirmPassword.Size = new System.Drawing.Size(91, 13);
+            this.lblConfirmPassword.TabIndex = 6;
+            this.lblConfirmPassword.Text = "Confirm Password";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(100, 86);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(180, 20);
+            this.txtEmail.TabIndex = 10;
+            this.toolTip1.SetToolTip(this.txtEmail, "This Email address will be used for authentication and in the event you forget yo" +
+        "ur password\r\n");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Account Creation";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 50;
+            this.toolTip1.AutoPopDelay = 5000;
+            this.toolTip1.InitialDelay = 50;
+            this.toolTip1.ReshowDelay = 10;
+            this.toolTip1.Tag = "";
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.Controls.Add(this.btnShowAccountPanel);
+            this.pnlLogin.Controls.Add(this.btnLogin);
+            this.pnlLogin.Controls.Add(this.hplkResetPassword);
+            this.pnlLogin.Location = new System.Drawing.Point(39, 254);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(426, 299);
+            this.pnlLogin.TabIndex = 6;
+            // 
+            // btnShowAccountPanel
+            // 
+            this.btnShowAccountPanel.ForeColor = System.Drawing.Color.Black;
+            this.btnShowAccountPanel.Location = new System.Drawing.Point(155, 202);
+            this.btnShowAccountPanel.Name = "btnShowAccountPanel";
+            this.btnShowAccountPanel.Size = new System.Drawing.Size(91, 23);
+            this.btnShowAccountPanel.TabIndex = 11;
+            this.btnShowAccountPanel.Text = "&Create Account";
+            this.btnShowAccountPanel.UseVisualStyleBackColor = true;
+            this.btnShowAccountPanel.Click += new System.EventHandler(this.btnShowAccountPanel_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(485, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmCreationLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 450);
+            this.Controls.Add(this.pnlLogin);
+            this.Controls.Add(this.pnlAccountCreation);
+            this.Name = "frmCreationLogin";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dbExpenseTrackerDataSet1)).EndInit();
+            this.pnlAccountCreation.ResumeLayout(false);
+            this.pnlAccountCreation.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private dbExpenseTrackerDataSet dbExpenseTrackerDataSet1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnCreateAccount;
+        private System.Windows.Forms.LinkLabel hplkResetPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Panel pnlAccountCreation;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblConfirmPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label lblErrorEmail;
+        private System.Windows.Forms.Label lblErrorPassword;
+        private System.Windows.Forms.Panel pnlLogin;
+        private System.Windows.Forms.Button btnShowAccountPanel;
+        private System.Windows.Forms.CheckBox ckbxShowPassword;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Personal Expense Tracker/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Personal_Expense_Tracker
+{
+    public partial class frmCreationLogin : Form
+    {
+        const int keySize = 128;
+        const int iterations = 350000;
+        HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA512;
+
+        public frmCreationLogin()
+        {
+            InitializeComponent();
+        }
+        private SqlConnection openDbConnection()
+        {
+            SqlConnection connString = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\falls\Documents\C# Portfolio projects\Personal Expense Tracker\Personal Expense Tracker\dbExpenseTracker.mdf;Integrated Security=True");
+            try
+            {
+                connString.Open();
+                return connString;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return null;
+            }
+        }
+
+        private void btnShowAccountPanel_Click(object sender, EventArgs e)
+        {
+            pnlAccountCreation.Visible = true;
+            pnlLogin.Visible = false;
+        }
+
+        private bool validateAccountCreationInput()
+        {
+            bool _specialChars;
+            bool _numbers;
+            bool _uppercase;
+            bool _validEmail;
+            string invalidAccountDetails = "";
+
+            MatchCollection numberInPassword = Regex.Matches(txtPassword.Text, @"[0-9]+");
+            MatchCollection specialCharInPassword = Regex.Matches(txtPassword.Text, @"[^a-zA-Z0-9\s]");
+            MatchCollection upperInPassword = Regex.Matches(txtPassword.Text, @"[A-Z]");
+            MatchCollection validAtEmail = Regex.Matches(txtEmail.Text, @"[/@]");
+            MatchCollection validDotEmail = Regex.Matches(txtEmail.Text, @"[/.]");
+
+            _numbers = numberInPassword.Count > 0 ? true : false;
+            _specialChars = specialCharInPassword.Count > 0 ? true : false;
+            _uppercase = upperInPassword.Count > 0 ? true : false;
+            _validEmail = (validAtEmail.Count == 1) && (validDotEmail.Count == 1) ? true : false;
+
+            invalidAccountDetails += !_numbers ? "Password must include Numerical values between 0-9\n" : "";
+            invalidAccountDetails += !_specialChars ? "Password must include Special characters\n" : "";
+            invalidAccountDetails += !_uppercase ? "Password must include an Upper case letter between A-Z\n" : "";
+
+            if (invalidAccountDetails != "")
+            {
+                lblErrorPassword.Text = invalidAccountDetails;
+                lblErrorPassword.Visible = true;
+            }
+            else
+                lblErrorPassword.Visible = false;
+
+            invalidAccountDetails = "";
+            invalidAccountDetails += !_validEmail ? "Email is not Valid\n" : "";
+
+            if (invalidAccountDetails != "")
+            {
+                lblErrorEmail.Text = invalidAccountDetails;
+                lblErrorEmail.Visible = true;
+            }
+            else
+                lblErrorEmail.Visible = false;
+
+            return ((_specialChars = true) && (_validEmail = true) && (_uppercase = true) && (_numbers = true)) ? true : false;
+        }
+        
+        private string hashPassword(string password, out byte[] saltKey)
+        {
+            new RNGCryptoServiceProvider().GetBytes(saltKey = new byte[keySize]);
+            Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, saltKey, iterations);
+
+            byte[] hashKey = pbkdf2.GetBytes(keySize);
+            byte[] hashPasswordBytes = new byte[(keySize * 2)];
+            Array.Copy(saltKey, 0, hashPasswordBytes, 0, keySize);
+            Array.Copy(hashKey, 0, hashPasswordBytes, keySize, (keySize*2));
+
+            string hashedPassword = Convert.ToBase64String(hashPasswordBytes);
+            return hashedPassword;
+        }
+        private void btnCreateAccount_Click(object sender, EventArgs e)
+        {
+            if(validateAccountCreationInput())
+            {
+                SqlConnection connString = openDbConnection();
+                try
+                {
+                    
+                    using (SqlCommand createUserQuery = new SqlCommand("INSERT INTO Users (Username,Password,Email) Values(@username,@password,@email)",connString))
+                    {
+                        createUserQuery.Parameters.AddWithValue("@username", txtUsername.Text);
+                        createUserQuery.Parameters.AddWithValue("@password", txtPassword.Text);
+                        createUserQuery.Parameters.AddWithValue("@email", txtEmail.Text);
+                        createUserQuery.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                connString.Close();
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            if(txtPassword.Text != txtConfirmPassword.Text)
+            {
+                lblErrorPassword.Text = "Passwords do not match";
+                lblErrorPassword.Visible = true;
+                txtConfirmPassword.BackColor = Color.Red;
+                btnCreateAccount.Enabled = false;
+            }
+        }
+
+        private void txtConfirmPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (txtConfirmPassword.Text == txtPassword.Text)
+            {
+                lblErrorPassword.Visible = false;
+                txtConfirmPassword.BackColor = SystemColors.Window;
+                btnCreateAccount.Enabled = true;
+            }
+        }
+
+        private void ckbxShowPassword_Click(object sender, EventArgs e)
+        {
+            txtConfirmPassword.UseSystemPasswordChar = ckbxShowPassword.Checked ? false : true;
+            txtPassword.UseSystemPasswordChar = ckbxShowPassword.Checked ? false : true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string _password = hashPassword(txtPassword.Text, out byte[] saltKey);
+            MessageBox.Show(_password);
+        }
+    }
+}
